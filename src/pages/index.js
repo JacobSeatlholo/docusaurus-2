18cd7f9da16ae78d31e1611c78127033110626ad
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-02-26 12:39:41 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,22 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
+    title: <>Easy to Register</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
+        This page was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
       </>
     ),
   },
   {
-    title: <>Focus on What Matters</>,
+    title: <>Why Accessibility Matters</>,
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        People with disabilities should be able to perceive, understand, navigate, and interact with, and contribute to your website.
+    Tourism and travel is something that should be enjoyed by everyone i.e. young, elderly and people with disabilities.
       </>
     ),
   },
